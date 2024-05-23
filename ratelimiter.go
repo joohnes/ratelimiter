0d63ab357f2114cff5c2b70e507f887d3beb95f6
joohnes <kubapienkowski@gmail.com>
2024-05-23 21:59:50 +0200

    add option to set minimum time between each use in burst

--- ratelimiter.go
+++ ratelimiter.go
@@ -13,26 +13,54 @@ var (
 )
 
 type RateLimiter struct {
-	mu       sync.Mutex
-	burst    uint
-	maxBurst uint
+	mu sync.Mutex
+
+	burst         uint
+	maxBurst      uint
+	burstInterval time.Duration
+
+	expiry   time.Time
 	interval time.Duration
 	ticker   *time.Ticker
 }
 
-func NewRateLimiter(ctx context.Context, interval time.Duration) (*RateLimiter, error) {
-	return NewRateLimiterWithBurst(ctx, interval, 1)
+// RateLimiterOptions is a struct that holds the options for the RateLimiter
+//
+// # BurstAmount is the amount of uses that can be used in a burst
+//
+// # BurstInterval is the minimum time between each use in a burst
+//
+// # Interval is the time to wait for the burst to refill by one
+type RateLimiterOptions struct {
+	BurstAmount   int
+	BurstInterval time.Duration
+	Interval      time.Duration
 }
 
-func NewRateLimiterWithBurst(ctx context.Context, interval time.Duration, burst int) (*RateLimiter, error) {
-	if burst < 1 {
+func NewRateLimiter(ctx context.Context, interval time.Duration) (*RateLimiter, error) {
+	opts := RateLimiterOptions{
+		BurstAmount:   1,
+		BurstInterval: interval,
+		Interval:      interval,
+	}
+	return NewRateLimiterWithBurst(ctx, opts)
+}
+
+func NewRateLimiterWithBurst(ctx context.Context, opts RateLimiterOptions) (*RateLimiter, error) {
+	if opts.BurstAmount < 1 {
 		return nil, ErrBurst
 	}
-	if interval < 1 {
+	if opts.Interval < 1 {
 		return nil, ErrInterval
 	}
 
-	rl := &RateLimiter{burst: uint(burst), maxBurst: uint(burst), interval: interval}
+	rl := &RateLimiter{
+		burst:         uint(opts.BurstAmount),
+		maxBurst:      uint(opts.BurstAmount),
+		interval:      opts.Interval,
+		burstInterval: opts.BurstInterval,
+		expiry:        time.Now(),
+	}
 	rl.ticker = time.NewTicker(rl.interval)
 
 	go func() {
@@ -56,7 +84,8 @@ func (rl *RateLimiter) Use() bool {
 	rl.mu.Lock()
 	defer rl.mu.Unlock()
 
-	if rl.burst > 0 {
+	if rl.burst > 0 && rl.expiry.Before(time.Now()) {
+		rl.expiry = time.Now().Add(rl.burstInterval)
 		rl.burst -= 1
 		rl.ticker.Reset(rl.interval)
 		return true
